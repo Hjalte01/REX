805d5a1a689eb51aa7fab22574b6751e4f8ce542
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-04 12:08:05 +0200

    720 turn

--- week_1/square.py
+++ week_1/square.py
@@ -7,7 +7,7 @@ arlo = robot.Robot()
 
 print("Running ...")
 
-left_motor_diff = 1
+left_motor_diff = 0.96
 
 # # send a go_diff command to drive forward
 # leftSpeed = 53*left_motor_diff
@@ -26,7 +26,7 @@ rightSpeed = 53
 print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
 
 # Wait a bit while robot moves forward
-sleep(0.94*4)
+sleep(0.94*8)
 
 # send a stop command
 print(arlo.stop())
