8e1767db8d38bbb36a8a20bf2097c29084818d66
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-04 13:27:00 +0200

    square

--- week_1/square.py
+++ week_1/square.py
@@ -16,31 +16,35 @@ left_motor_diff = 0.94
 leftSpeed = 53*left_motor_diff
 rightSpeed = 53
 
-for (i) in range(1):
-    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 
+def forward(len):
+    distance = 3 * len # in meters
+
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
     # Wait a bit while robot moves forward
-    sleep(3)
+    sleep(distance)
 
     # send a stop command
     print(arlo.stop())
 
-    # # send a go_diff command to turn left
-    # print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+def turn_left(degree):
+    constant_90_degree = 0.94 / 90
 
-    # # Wait a bit while robot turns left
-    # sleep(0.94)
+    # send a go_diff command to turn left
+    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+
+    # Wait a bit while robot turns left
+    sleep(constant_90_degree*degree)
+    
+
+def square():
+    for i in range(4):
+        forward(1)
+        turn_left(90)
+
+
+# call the square function
+square()
 
 # send a stop command
 print(arlo.stop())
-
-# # send a go_diff command to turn left
-# leftSpeed = 53*left_motor_diff
-# rightSpeed = 53
-# print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
-
-# # Wait a bit while robot moves forward
-# sleep(0.94*8)
-
-# # send a stop command
-# print(arlo.stop())
