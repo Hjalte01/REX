b7014fe2eb9c4531cfe30837ea578a10d2a45b09
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-04 14:12:25 +0200

    s

--- week_1/square.py
+++ week_1/square.py
@@ -10,7 +10,7 @@ sleep(2)
 
 print("Running ...")
 
-left_motor_diff = 0.90
+left_motor_diff = 0.88
 
 # send a go_diff command to drive forward
 leftSpeed = 53*left_motor_diff
@@ -20,11 +20,6 @@ rightSpeed = 53
 def forward(len):
     distance = 3 * len # in meters
 
-    # acceleration configuration
-    print(arlo.go_diff(leftSpeed/10, rightSpeed*10, 1, 1))
-    # Wait a bit while robot moves forward
-    sleep(0.8)
-
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
     # Wait a bit while robot moves forward
     sleep(distance)
@@ -33,7 +28,7 @@ def forward(len):
     print(arlo.stop())
 
 def turn_left(degree):
-    constant_90_degree = 0.94 / 90
+    constant_90_degree = 0.92 / 90
 
     # send a go_diff command to turn left
     print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
