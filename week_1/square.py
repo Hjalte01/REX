acaa39f46e2f11766376d242ee2ced5964ff8fb2
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-04 11:43:33 +0200

    mdif

--- week_1/square.py
+++ week_1/square.py
@@ -7,7 +7,21 @@ arlo = robot.Robot()
 
 print("Running ...")
 
-left_motor_diff = 0.98
+left_motor_diff = 0.96
+
+# send a go_diff command to drive forward
+leftSpeed = 53*left_motor_diff
+rightSpeed = 53
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+
+# Wait a bit while robot moves forward
+sleep(3)
+
+# send a stop command
+print(arlo.stop())
+
+# Wait a bit while robot moves forward
+sleep(3)
 
 # send a go_diff command to drive forward
 leftSpeed = 53*left_motor_diff
