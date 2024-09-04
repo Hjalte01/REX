0a5ba7745ec053795f3a18d43f0e120d05909ddd
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-04 12:13:52 +0200

    forward again

--- week_1/square.py
+++ week_1/square.py
@@ -9,24 +9,24 @@ print("Running ...")
 
 left_motor_diff = 0.96
 
-# # send a go_diff command to drive forward
-# leftSpeed = 53*left_motor_diff
-# rightSpeed = 53
-# print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-
-# # Wait a bit while robot moves forward
-# sleep(3)
-
-# # send a stop command
-# print(arlo.stop())
-
-# send a go_diff command to turn left
+# send a go_diff command to drive forward
 leftSpeed = 53*left_motor_diff
 rightSpeed = 53
-print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 
 # Wait a bit while robot moves forward
-sleep(0.94*8)
+sleep(3)
 
 # send a stop command
 print(arlo.stop())
+
+# # send a go_diff command to turn left
+# leftSpeed = 53*left_motor_diff
+# rightSpeed = 53
+# print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+
+# # Wait a bit while robot moves forward
+# sleep(0.94*8)
+
+# # send a stop command
+# print(arlo.stop())
