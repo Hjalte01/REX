19aa88349964d5bde6edffe8b5002d90b5d8b283
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-04 10:11:03 +0200

    beginning of square ex.

---
+++ week_1/square.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+
+print("Running ...")
+
+# send a go_diff command to drive forward
+leftSpeed = 64
+rightSpeed = 64
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+
+# Wait a bit while robot moves forward
+sleep(3)
+
+# send a stop command
+print(arlo.stop())
+
+# Wait a bit before next command
+sleep(0.041)
+
+# send a go_diff command to turn 90 degrees to the left
+leftSpeed = 64
+rightSpeed = 32
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+
+# Wait a bit while robot turns
+sleep(3)
+
+# send a stop command
+print(arlo.stop())
