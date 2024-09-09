cb105b14d259aa2d0e7e0c26e3272dde30651372
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-09 15:18:13 +0200

    week 2 + moved robot.py out of week 1 folder

---
+++ week_2/obstacle_avoidance.py
@@ -0,0 +1,37 @@
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+
+# sleep for 2 seconds
+sleep(2)
+
+print("Running ...")
+
+left_motor_diff = 0.875
+
+# send a go_diff command to drive forward
+leftSpeed = 40*left_motor_diff
+rightSpeed = 40
+
+
+def forward(len):
+    distance = 4.5 * len # in meters
+
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+    # Wait a bit while robot moves forward
+    sleep(distance)
+
+    # send a stop command
+    print(arlo.stop())
+
+def turn_left(degree):
+    constant_90_degree = 1.5 / 90
+
+    # send a go_diff command to turn left
+    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+
+    # Wait a bit while robot turns left
+    sleep(constant_90_degree*degree)
