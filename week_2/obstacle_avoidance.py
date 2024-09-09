2a30235f9d88dc2913f2a1f4b9f73ccda6df3d34
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-09 16:27:15 +0200

    ss

--- week_2/obstacle_avoidance.py
+++ week_2/obstacle_avoidance.py
@@ -64,9 +64,9 @@ def drive_around_and_detect_obstacle():
             print(arlo.stop())
         elif arlo.read_front_ping_sensor() < 200:
             print("obstacle detected in front")
-            turn_left(180)
+            turn_left(270)
             print(arlo.stop())
-            
+
 
 
 drive_around_and_detect_obstacle()
