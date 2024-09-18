79748a70584de3a4c29b930680f61adb079c9f4f
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-18 16:23:38 +0200

    change from streaming to image capturing

--- week_3/find_landmark.py
+++ week_3/find_landmark.py
@@ -1,89 +1,64 @@
 # This file is used to find the landmarks (Aruco Marker) in the image plane and drive the robot to the landmark 
 # by computing the distance and angle between the robot and the landmark.
 
-# Import the required libraries
 import cv2 # Import the OpenCV library
 from cv2 import aruco
+import time
+import sys
 import numpy as np
+from pprint import *
 
+try:
+    import picamera2
+    print("Camera.py: Using picamera2 module")
+except ImportError:
+    print("Camera.py: picamera2 module not available")
+    exit(-1)
 
-def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=30):
-    """Utility function for setting parameters for the gstreamer camera pipeline"""
-    return (
-        "libcamerasrc !"
-        "videobox autocrop=true !"
-        "video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        "videoconvert ! "
-        "appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-        )
-    )
-
-
-print("OpenCV version = " + cv2.__version__)
-
-# Print the GStreamer pipeline
-pipeline = gstreamer_pipeline()
-print(f"Using GStreamer pipeline: {pipeline}")
+# print("OpenCV version = " + cv2.__version__)
 
 # Open a camera device for capturing
-cam = cv2.VideoCapture(pipeline, apiPreference=cv2.CAP_GSTREAMER)
+imageSize = (1280, 720)
+FPS = 30
+cam = picamera2.Picamera2()
+frame_duration_limit = int(1/FPS * 1000000) # Microseconds
+# Change configuration to set resolution, framerate
+picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
+                                                            controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
+                                                            queue=False)
+cam.configure(picam2_config) # Not really necessary
+cam.start(show_preview=False)
 
-if not cam.isOpened():  # Error
-    print("Could not open camera with GStreamer pipeline")
-    print("Trying to open default camera with index 0")
-    cam = cv2.VideoCapture(0)
-    if not cam.isOpened():
-        print("Could not open default camera either")
-        exit(-1)
-    else:
-        print("Default camera opened successfully")
-else:
-    print("Camera opened successfully with GStreamer pipeline")
+time.sleep(1)  # wait for camera to setup
 
-# # Open a window
-# WIN_RF = "Example 1"
-# cv2.namedWindow(WIN_RF)
-# cv2.moveWindow(WIN_RF, 100, 100)
 
-# Get the predefined dictionary
+# Capture an image from the camera
+image = cam.capture_array("main")
+
+print("Image shape: ", image.shape)
+
+
 img_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250) # As per the assignment
 
+# Detect the markers in the images
+corners, ids, _ = aruco.detectMarkers(image, img_dict)
 
-# Process video frames and check for Aruco markers
 
-while cv2.waitKey(4) == -1: # Wait for a key pressed event
-    retval, frameReference = cam.read() # Read frame
-    
-    if not retval: # Error
-        print(" < < <  Game over!  > > > ")
-        exit(-1)
-    
-    print("Detecting markers in the image")
-    # Detect the markers in the images
-    corners, ids, _ = aruco.detectMarkers(frameReference, img_dict)
+if corners == None:
+    print("no corners detected")
+    sys.exit()
 
-    # If no markers are detected
-    if not corners:
-        print("No corners detected")
-        continue
+# Get the camera matrix and distortion coefficients
+cam_matrix = np.zeros((3, 3))
+coeff_vector = np.zeros(5)
 
-    # Get the camera matrix and distortion coefficients
-    cam_matrix = np.zeros((3, 3))
-    coeff_vector = np.zeros(5)
+focal_length = 1694.0
+cam_matrix[0, 0] = focal_length
 
-    print("Estimated pose of the markers")
+marker_length = 0.15 # meters
 
-    # Estimate the pose of the markers in the image
-    rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, 0.15, cam_matrix, coeff_vector)
+# Estimate the pose of the markers
+rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, marker_length, cam_matrix, coeff_vector)
 
-    print("rvecs: ", rvecs)
-    print("tvecs: ", tvecs)
-
-    
-    
-
-# Finished successfully
+print("rvecs: ", rvecs)
+print("tvecs: ", tvecs)
