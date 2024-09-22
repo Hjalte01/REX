5f187a47e51edbc06cda0424f141d02e7f3fa24d
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-22 14:25:35 +0200

    First attempt on finding landmark and driving towards it

--- week_3/find_landmark.py
+++ week_3/find_landmark.py
@@ -9,6 +9,7 @@ import numpy as np
 from pprint import *
 
 import os, sys
+from time import sleep
 
 # get for the robot module from the parent directory
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -38,25 +39,11 @@ cam.start(show_preview=False)
 
 time.sleep(1)  # wait for camera to setup
 
-
+    
 # Capture an image from the camera
 image = cam.capture_array("main")
 image_width = image.shape[1]
 image_height = image.shape[0]
-
-print("Image shape: ", image.shape)
-
-
-img_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250) # As per the assignment
-
-# Detect the markers in the images
-corners, ids, _ = aruco.detectMarkers(image, img_dict)
-
-print("corners: ", corners)
-
-if corners == None:
-    print("no corners detected")
-    sys.exit()
 
 # Get the camera matrix and distortion coefficients
 cam_matrix = np.zeros((3, 3))
@@ -73,16 +60,140 @@ cam_matrix[2, 2] = 1.0
 
 marker_length = 0.15 # meters
 
-# Estimate the pose of the markers
-rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, marker_length, cam_matrix, coeff_vector)
+# get the dictionary for the aruco markers
+img_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250) # As per the assignment
 
-print("rvecs: ", rvecs)
-print("tvecs: ", tvecs)
 
-# Calculate the distance and angle between the robot and the landmark
+def get_landmark(cam, img_dict, cam_matrix, coeff_vector, marker_length):
+    """Get the landmark from the camera and return the distance and angle between the robot and the landmark"""
+    # Capture an image from the camera
+    image = cam.capture_array("main")
 
-distance = np.linalg.norm(tvecs[0])
-angle = np.arctan2(tvecs[0][0][0], tvecs[0][0][2])
+    # Detect the markers in the images
+    corners, ids, _ = aruco.detectMarkers(image, img_dict)
 
-print("Distance: ", distance)
-print("Angle: ", angle)
+    print("corners: ", corners)
+
+    if corners == None:
+        print("no corners detected")
+        return None
+
+    # Estimate the pose of the markers
+    rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, marker_length, cam_matrix, coeff_vector)
+
+    print("rvecs: ", rvecs)
+    print("tvecs: ", tvecs)
+
+    # Calculate the distance and angle between the robot and the landmark
+
+    distance = np.linalg.norm(tvecs[0])
+    angle = np.arctan2(tvecs[0][0][0], tvecs[0][0][2])
+
+    print("Distance: ", distance)
+    print("Angle: ", angle)
+
+    return distance, angle
+
+
+
+def search_for_landmark(cam, img_dict, cam_matrix, coeff_vector, marker_length, arlo, leftSpeed, rightSpeed):
+    """
+    Turn around until the landmark is found and return the distance and angle between the robot and the landmark
+    """
+    turn_speed_constant = 0.5
+    while True:
+        distance, angle = get_landmark(cam, img_dict, cam_matrix, coeff_vector, marker_length)
+        if distance != None:
+            return distance, angle
+        else:
+            # Turn around
+            print(arlo.go_diff(leftSpeed*turn_speed_constant, rightSpeed*turn_speed_constant, 0, 1))
+            sleep(1)
+            print(arlo.stop())
+            sleep(1)
+
+
+# Turn until the robot is facing the landmark
+def turn_towards_landmark(angle, arlo, leftSpeed, rightSpeed):
+    """
+    Turn the robot until it is facing the landmark
+    """
+    while angle > 0.1:
+        print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+        sleep(1)
+        print(arlo.stop())
+        sleep(1)
+    
+
+# Correct the angle of the robot while driving towards the landmark
+def correct_angle(angle, arlo, leftSpeed, rightSpeed):
+    """
+    Correct the angle of the robot while driving towards the landmark by changing the speed of the motors
+    """
+    # Left turn correction
+    slow_speed = 0.9
+    if angle > 0.1:
+        print(arlo.go_diff(leftSpeed*slow_speed, rightSpeed, 1, 1))
+        sleep(1)
+        print(arlo.stop())
+        sleep(1)
+    # Right turn correction
+    elif angle < -0.1:
+        print(arlo.go_diff(leftSpeed, rightSpeed*slow_speed, 1, 1))
+        sleep(1)
+        print(arlo.stop())
+        sleep(1)
+
+
+def drive_towards_landmark(distance, angle, arlo, leftSpeed, rightSpeed):
+    """
+    Drive the robot towards the landmark and keep updating the distance and angle between the robot and the landmark
+    """
+    # Turn until the robot is facing the landmark
+    turn_towards_landmark(angle, arlo, leftSpeed, rightSpeed)
+    
+    # Drive towards the landmark
+    while distance > 0.1:
+        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+        sleep(1)
+        print(arlo.stop())
+        sleep(1)
+
+        # Update the distance and angle between the robot and the landmark
+        distance, angle = get_landmark(cam, img_dict, cam_matrix, coeff_vector, marker_length)
+
+        if distance == None:
+            print("Landmark lost")
+            # search for the landmark
+            distance, angle = search_for_landmark(cam, img_dict, cam_matrix, coeff_vector, marker_length, arlo, leftSpeed, rightSpeed)
+            turn_towards_landmark(angle, arlo, leftSpeed, rightSpeed)
+        
+        # Correct the angle of the robot while driving towards the landmark
+        correct_angle(angle, arlo, leftSpeed, rightSpeed)
+
+    return distance, angle
+
+
+def main():
+    # initialize the robot
+    arlo = robot.Robot()
+
+    # sleep for 2 seconds
+    sleep(2)
+
+    # set the speed of the robot
+    left_motor_diff = 0.875
+    leftSpeed = 40*left_motor_diff
+    rightSpeed = 40
+
+    # search for the landmark, if landmark is lost during the search, the robot will turn around until the landmark is found again
+    distance, angle = search_for_landmark(cam, img_dict, cam_matrix, coeff_vector, marker_length, arlo, leftSpeed, rightSpeed)
+
+    # Drive towards the landmark
+    drive_towards_landmark(distance, angle, arlo, leftSpeed, rightSpeed)
+
+    print(arlo.stop())
+    sleep(1)
+
+    cam.stop()
+
