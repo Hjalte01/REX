a395f969070aa773a55a3f8ea45dd7add96bf0e6
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-09-23 17:24:41 +0200

    as

--- week_3/find_landmark.py
+++ week_3/find_landmark.py
@@ -74,7 +74,8 @@ def get_landmark(cam, img_dict, cam_matrix, coeff_vector, marker_length):
 
     print("corners: ", corners)
 
-    if len(ids) != 0:
+    # check if the markers are detected
+    if corners != None:
         # Estimate the pose of the markers
         rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, marker_length, cam_matrix, coeff_vector)
 
