b0610785591135474f2219d02d3b0c4fa4af7ed3
hjalte01 <hjalte@bjoernstrup.net>
2024-10-09 12:30:03 +0200

    c

--- week_4/src/robot.py
+++ week_4/src/robot.py
@@ -27,7 +27,7 @@ class Robot(object):
         self.leftSpeed = 40*self.left_motor_diff
         self.rightSpeed = 40
         self.forward_to_meter = 4.5
-        self.turn_1_degree = 1.37 / 90
+        self.turn_1_degree = 1.36 / 90
         self.current_angle = 0
         # out changes
         
